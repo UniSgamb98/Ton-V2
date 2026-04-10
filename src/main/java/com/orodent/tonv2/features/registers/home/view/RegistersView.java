94ee538c259a40a52081fb7332c00827ff4dc641
UniSgamb98 <72046871+UniSgamb98@users.noreply.github.com>
2026-04-10 16:58:35 +0200

    Add Articolo and Lotto fields to Registers view

--- src/main/java/com/orodent/tonv2/features/registers/home/view/RegistersView.java
+++ src/main/java/com/orodent/tonv2/features/registers/home/view/RegistersView.java
@@ -2,21 +2,40 @@ package com.orodent.tonv2.features.registers.home.view;
 
 import com.orodent.tonv2.core.components.AppHeader;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class RegistersView extends VBox {
     private final AppHeader header;
+    private final TextField articleField;
+    private final TextField lotField;
 
     public RegistersView() {
         header = new AppHeader("Registri");
+        articleField = new TextField();
+        lotField = new TextField();
 
         setSpacing(20);
         setPadding(new Insets(20));
 
-        getChildren().add(header);
+        VBox articleBox = new VBox(8, new Label("Articolo"), articleField);
+        VBox lotBox = new VBox(8, new Label("Lotto"), lotField);
+        HBox filtersBox = new HBox(20, articleBox, lotBox);
+
+        getChildren().addAll(header, filtersBox);
     }
 
     public AppHeader getHeader() {
         return header;
+    }
+
+    public TextField getArticleField() {
+        return articleField;
+    }
+
+    public TextField getLotField() {
+        return lotField;
     }
 }
